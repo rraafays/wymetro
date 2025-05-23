4c0641593a1b40b5f9fdb3ede693333009d8a3bd
ramraafay <rafs@ramtracking.com>
2025-05-23 16:07:00 +0100

    chore: flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs =
+            with pkgs;
+            [
+              cargo
+              rustc
+              rustfmt
+              rust-analyzer
+            ]
+            ++ lib.optional stdenv.isDarwin libiconv;
+        };
+      }
+    );
+}
